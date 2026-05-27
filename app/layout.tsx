7e816aaa303bd54cbc6fef95b4804a89b91c089a
Gabriela1011 <lionte.gabriela@yahoo.com>
2026-05-27 23:46:57 +0300

    refactor(layout): move Toaster component inside body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -30,7 +31,10 @@ export default function RootLayout({
       lang="ro"
       className={`${playfair.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster richColors position="top-center" />
+      </body>
     </html>
   );
 }
